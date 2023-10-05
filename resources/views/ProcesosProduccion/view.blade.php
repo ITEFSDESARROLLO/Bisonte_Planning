8c2030ba746fa35debbbb77ab827bf388415906e
ITEFSDESARROLLO <sistemas@itefs.com>
2023-10-05 11:47:46 -0500

    Se hacen mejoras de interfaz

--- resources/views/ProcesosProduccion/view.blade.php
+++ resources/views/ProcesosProduccion/view.blade.php
@@ -1,106 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="app-page-title m-0">
-                        <div class="page-title-wrapper">
-                            <div class="page-title-heading">
-                                <div class="page-title-icon">
-                                <img src="images/procesoproduccion.png"/>
-                                </div>
-                                <div>Ordenes de producción
-                                </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="app-page-title m-0">
+                    <div class="page-title-wrapper">
+                        <div class="page-title-heading">
+                            <div class="page-title-icon">
+                                <img src="images/procesoproduccion.png" alt="Orden de Producción" />
                             </div>
-                            <div class="page-title-actions">
-                                <a href="ProcesosProduccion/create" type="button" data-toggle="tooltip" title=""
-                                   data-placement="bottom"
-                                   class="btn-primary mr-3 btn " data-original-title="Crear Orden">
-                                    Nueva Orden
-                                </a>
-                            </div>
-                          <form action="{{ route('upload.excel.controller') }}" method="POST" enctype="multipart/form-data">
+                            <div>Ordenes de producción</div>
+                        </div>
+                        <div class="page-title-actions">
+                            <a href="ProcesosProduccion/create" type="button" data-toggle="tooltip" title=""
+                                data-placement="bottom" class="btn-primary mr-3 btn " data-original-title="Crear Orden">
+                                Nueva Orden
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('upload.excel.controller') }}" method="POST" enctype="multipart/form-data">
                         @csrf
-                        <input type="file" name="excel_file" accept=".xls,.xlsx">
-                        <button type="submit">Subir archivo</button>
-                        </form>
-                          <form action="{{ route('filtrar.produccion') }}" method="GET">
-                         <div class="form-group">
-                           <label for="fecha_inicio">Inicio:</label>
-                            <input type="date" id="fecha_inicio" name="fecha_inicio">
-                            <label for="fecha_fin">Fin:</label>
-                            <input type="date" id="fecha_fin" name="fecha_fin">
-                             <button type="submit">Filtrar</button>
-                            </form>
-                            <form action="{{ route('quitar.filtro') }}" method="GET">
-                             <button type="submit">Quitar</button>
-                            </form>
+                        <div class="mb-3">
+                            <input type="file" name="excel_file" accept=".xls,.xlsx">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Subir archivo</button>
+                    </form>
+
+                    <form action="{{ route('filtrar.produccion') }}" method="GET" class="mt-3">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="fecha_inicio">Inicio:</label>
+                                <input type="date" id="fecha_inicio" name="fecha_inicio" class="form-control">
                             </div>
+                            <div class="col-md-3">
+                                <label for="fecha_fin">Fin:</label>
+                                <input type="date" id="fecha_fin" name="fecha_fin" class="form-control">
                             </div>
-                    <div class="card-body">
-                        <table class="mb-0 table">
+                            <div class="col-md-3 mt-4">
+                                <button type="submit" class="btn btn-primary">Filtrar</button>
+                            </div>
+                            <div class="col-md-3 mt-4">
+                                <button type="submit" formaction="{{ route('quitar.filtro') }}" class="btn btn-danger">Quitar
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <div class="table-responsive mt-4">
+                        <table class="table">
                             <thead>
-                            <tr>
-                           <!---<th>#</th>-->
-                                <th>Lote</th>
-                                <th>Cantidad</th>
-                                <th>Color</th>
-                                <th>Linea de Produccion</th>
-                                <th>Inicio Fecha</th>
-                                <th>Final Fecha</th>
-                                <th>Indentificador</th>
-                                <th>Producto</th>
-                                <th></th>
-                            </tr>
+                                <tr>
+                                    <th>Lote</th>
+                                    <th>Cantidad</th>
+                                    <th>Color</th>
+                                    <th>Linea de Produccion</th>
+                                    <th>Inicio Fecha</th>
+                                    <th>Final Fecha</th>
+                                    <th>Indentificador</th>
+                                    <th>Producto</th>
+                                    <th>Acciones</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            @foreach($data as $d)
-                                <tr class="{{($d->is_range === 'current') ?
-                                    'row-danger-border':
-                                       (($d->is_range === 'next_week') ? 'row-warning-border': '')
-                                    }}">
-                                  <!--  <th scope="row">{{$d->id}}</th>-->
+                                @foreach($data as $d)
+                                <tr class="{{($d->is_range === 'current') ? 'row-danger-border': (($d->is_range === 'next_week') ? 'row-warning-border': '')}}">
                                     <td>{{$d->name}}</td>
                                     <td>{{$d->cantidad}}</td>
                                     <td>
-                                        <span class="badge badge-info"
-                                              style="background-color: {{$d->color}}">{{$d->color}}</span>
+                                        <span class="badge badge-info" style="background-color: {{$d->color}}">{{$d->color}}</span>
                                     </td>
-                                    <!-- Esta linea llama la tabla de LienasProduccion  por medio del ID y las muestra en la vista
-                                    Si se quiere mostrar por id se pone el campo de la DB  en: INT-->
                                     <td>{{($d->LineasProduccion) ? $d->LineasProduccion->title : 'N/A'}}</td>
-                                   <!-- <td>{{$d->projects_id}}</td>Esta Linea  muesta lo propio de la tabla en ese campo
-                                    Si se quiere mostrar el campo propio del la Tabla se pone en : VARCHAR-->
                                     <td>{{$d->start_at}}</td>
                                     <td>{{$d->deadline_at}}</td>
                                     <td>{{($d->Producto) ? $d->Producto->codigo : 'N/A'}}</td>
                                     <td>{{($d->Producto) ? $d->Producto->name : 'N/A'}}</td>
                                     <td>
                                         <div class="d-flex">
-                                            <a href="ProcesosProduccion/{{$d->id}}/edit" class="btn btn-sm"
-                                               data-toggle="tooltip" title=""
-                                               data-original-title="Editar">
-                                               <img src="images/editar.png" />
+                                            <a href="ProcesosProduccion/{{$d->id}}/edit" class="btn btn-sm" data-toggle="tooltip" title=""
+                                                data-original-title="Editar">
+                                                <img src="images/editar.png" alt="Editar" />
                                             </a>
-                                            <div class="d-flex">
-                                        <a href="ProcesosProduccion/{{$d->id}}" class="btn btn-sm"
-                                           data-toggle="tooltip" title=""
-                                           data-original-title="Ver">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
+                                            <a href="ProcesosProduccion/{{$d->id}}" class="btn btn-sm" data-toggle="tooltip" title=""
+                                                data-original-title="Ver">
+                                                <i class="fas fa-eye"></i>
+                                            </a>
                                             <form method="POST" action="ProcesosProduccion/{{$d->id}}">
                                                 {{ csrf_field() }}
                                                 {{ method_field('DELETE') }}
                                                 <button class="btn-sm btn">
-                                                <img src="images/eliminar.png" />
+                                                    <img src="images/eliminar.png" alt="Eliminar" />
                                                 </button>
                                             </form>
                                         </div>
                                     </td>
                                 </tr>
-                            @endforeach
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -108,6 +107,5 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
-
-
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-
-    <div class="container">
-
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-
-                    <div class="card-header">Menu Principal</div>
-
-                    <div class="card-body">
-                        <div class="col-md-12 row m-0">
-                            <div class="col-4 mb-4 over-block-card card p-3 text-center">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header text-center">Menu Principal</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4 mb-4">
+                            <div class="over-block-card card p-3 text-center">
                                 <a class="btn btn-default" href="usuarios">
-                                    <img src="images/usuarioagregar.png" />
+                                    <img src="images/usuarioagregar.png" class="mb-2" alt="Usuarios" />
                                     <div class="nombre_modulos">Usuarios</div>
                                 </a>
                             </div>
-
-                            <div class="col-4 mb-4 over-block-card card p-3 text-center">
+                        </div>
+                        <div class="col-md-4 mb-4">
+                            <div class="over-block-card card p-3 text-center">
                                 <a class="btn btn-default" href="Producto">
-                                    <img src="images/agregar.png" />
+                                    <img src="images/agregar.png" class="mb-2" alt="Productos" />
                                     <div class="nombre_modulos">Productos</div>
                                 </a>
                             </div>
-                            <div class="col-4 mb-4 over-block-card card p-3 text-center">
+                        </div>
+                        <div class="col-md-4 mb-4">
+                            <div class="over-block-card card p-3 text-center">
                                 <a class="btn btn-default" href="LineasProduccion">
-                                    <img src="images/configuracionmas.png" />
+                                    <img src="images/configuracionmas.png" class="mb-2" alt="Lineas de producción" />
                                     <div class="nombre_modulos">Lineas de producción</div>
                                 </a>
                             </div>
                         </div>
-                        <div class="col-md-12 row m-0">
-                            <div class="col-4 mb-4 over-block-card card p-3 text-center">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 mb-4">
+                            <div class="over-block-card card p-3 text-center">
                                 <a class="btn btn-default" href="ProcesosProduccion">
-                                    <img src="images/playlist.png" />
+                                    <img src="images/playlist.png" class="mb-2" alt="Ordenes de Producción" />
                                     <div class="nombre_modulos">Ordenes de Producción</div>
                                 </a>
                             </div>
-
-                            <div class="col-4 mb-4 over-block-card card p-3 text-center">
+                        </div>
+                        <!-- Descomenta este bloque si decides usarlo -->
+                        <!--
+                        <div class="col-md-4 mb-4">
+                            <div class="over-block-card card p-3 text-center">
                                 <a class="btn btn-default text-center" href="Asignaciones">
-                                    <img src="images/amas.png" />
-                                    <div class="nombre_modulos">Asignacion de Productos a Linea</div>
+                                    <img src="images/amas.png" class="mb-2" alt="Asignación de Productos a Linea" />
+                                    <div class="nombre_modulos">Asignación de Productos a Linea</div>
                                 </a>
                             </div>
-
-                            <div class="col-4 mb-4 over-block-card card p-3 text-center">
+                        </div>
+                        -->
+                        <div class="col-md-4 mb-4">
+                            <div class="over-block-card card p-3 text-center">
                                 <a class="btn btn-default" href="chart">
-                                    <img src="images/planing.png" />
+                                    <img src="images/planing.png" class="mb-2" alt="Planning Lineas de Producción" />
                                     <div class="nombre_modulos">Planning Lineas de Producción</div>
                                 </a>
                             </div>
@@ -59,6 +66,5 @@
             </div>
         </div>
     </div>
-
-
+</div>
 @endsection
